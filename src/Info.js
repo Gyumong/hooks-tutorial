8ffeab43499894585f000304bc4831f74d5e7961
Gyumong <rlaalsrb3559@naver.com>
2020-03-18 15:44:40 +0900

    useReducer

--- src/Info.js
+++ src/Info.js
@@ -1,30 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React, {useReducer} from 'react';
+
+function reducer(state, action){
+  return{
+    ...state,
+    [action.name]: action.value
+
+  };
+}
 
 const Info =()=>{
-  const [name, setName]=useState('');
-  const [nickname, setNickname]=useState('');
-  useEffect(()=>{
-    console.log('effect');
-    console.log(name);
-return ()=>{
-  console.log('clean up');
-  console.log(name);
+  const [state, dispatch]=useReducer(reducer, {
+    name:'',
+    nickname:''
+  });
+const {name, nickname} = state;
+const onChange= e=>{
+  dispatch(e.target);
 };
-},[name]);
-
-  const onChangeName=e=>{
-    setName(e.target.value);
-  };
-
-  const onChangeNickname=e=>{
-    setNickname(e.target.value);
-  };
 
   return(
     <div>
       <div>
-      <input value={name} onChange={onChangeName}/>
-      <input value={nickname} onChange={onChangeNickname}/>
+      <input name="name" onChange={onChange}/>
+      <input name="nickname" onChange={onChange}/>
       </div>
     <div>
     <div>
